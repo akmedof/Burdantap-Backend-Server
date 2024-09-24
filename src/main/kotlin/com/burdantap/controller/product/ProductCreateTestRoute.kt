bec9844b286ffba9d8d3dced4b4a0d0845f3056c
Aslan Ahmedov <aslan@Aslans-MacBook-Pro.local>
2024-09-24 22:23:03 +0400

    update multi file uploads json, txt and images

--- src/main/kotlin/com/burdantap/controller/product/ProductCreateTestRoute.kt
+++ src/main/kotlin/com/burdantap/controller/product/ProductCreateTestRoute.kt
@@ -1,3 +1,4 @@
+@file:OptIn(ExperimentalSerializationApi::class)
 
 package com.burdantap.controller.product
 
@@ -12,30 +13,55 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
 import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import java.io.File
 import javax.mail.Multipart
 import kotlin.io.use
 
+private val json = Json {
+    ignoreUnknownKeys = true
+    explicitNulls = true
+}
+
 internal fun Route.productCreateTestRoute() {
     post(ProductEndpoint.CreateTest.path) {
         val multipart = call.receiveMultipart()
         var fileName = ""
-        var productDto: ProductTestDto? = null
-        var file: File? = null
+        var productDto: ProductTestDto? = ProductTestDto()
+        val uploadDirPath = "uploads/store/blue"
+        val targetDir = File(uploadDirPath)
+
+        if (!targetDir.exists()) {
+            targetDir.mkdirs() // Gerekli dizin yapısını oluştur
+        }
         multipart.forEachPart { part ->
             if (part is PartData.FileItem) {
                 fileName = part.originalFileName ?: "uploadedFile"
-
-                if(fileName.contains(".json")) {
-                    file = File("uploads/$fileName")
-                    // Dosya içeriğini kaydet
-                    part.streamProvider().use { inputStream ->
-                        application.log.info("File read text: ${file!!.readText()}")
-                        file!!.outputStream().buffered().use { outputStream ->
-                            inputStream.copyTo(outputStream)
+                part.contentType?.let { contentType ->
+                    when (contentType) {
+                        ContentType.Application.Json -> {
+                            application.log.info("FILE TYPE: ${contentType}")
+                            application.log.info("FILE NAME: $fileName")
+                            part.provider().use { inputStream ->
+                                val jsonString = inputStream.readBytes().toString(Charsets.UTF_8) // JSON dosyasını string'e çevir
+                                productDto = json.decodeFromString<ProductTestDto>(jsonString) // JSON'u modele çevir
+                            }
                         }
-                        productDto = Json.decodeFromString<ProductTestDto>(file!!.readText())
+                        ContentType.Text.Plain -> {
+                            application.log.info("FILE TYPE: ${contentType}")
+                            application.log.info("FILE NAME: $fileName")
+                        }
+                        ContentType.Image.JPEG, ContentType.Image.PNG -> {
+                            application.log.info("FILE TYPE: ${contentType}")
+                            application.log.info("FILE NAME: $fileName")
+                            part.provider().use { inputStream ->
+                                val byteArray = inputStream.readBytes() // Görsel dosyasını bayt dizisine çevir
+                                File(targetDir, fileName).writeBytes(byteArray) // Bayt dizisini dosyaya yaz
+                                application.log.info("Image file uploaded: $fileName")
+                            }
+                        }
                     }
                 }
             }
